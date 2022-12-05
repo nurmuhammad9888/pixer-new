ec32a3420a52015dc9ba231b7156f41c4cf18ea5
Nurmuhammad <juaqobilovnurmuhammad9888@gmail.com>
2022-12-05 07:12:59 +0500

    Add animation

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const elNavbar = document.querySelector(".navbar-list-wrap");
 const elBurger = document.querySelector(".burger-btn");
 
+AOS.init();
+
 elBurger.addEventListener("click" , () =>{
     elNavbar.classList.toggle("navbar-list-wrap-show");
     elBurger.classList.toggle("burger-btn-show");
