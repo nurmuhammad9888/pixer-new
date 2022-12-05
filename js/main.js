e6503ad60e6788c70ff999adb019c88d8f119054
Nurmuhammad <juaqobilovnurmuhammad9888@gmail.com>
2022-12-05 06:47:46 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const elNavbar = document.querySelector(".navbar-list-wrap");
+const elBurger = document.querySelector(".burger-btn");
+
+elBurger.addEventListener("click" , () =>{
+    elNavbar.classList.toggle("navbar-list-wrap-show");
+    elBurger.classList.toggle("burger-btn-show");
+    document.body.classList.toggle("body-hidden");
+})
